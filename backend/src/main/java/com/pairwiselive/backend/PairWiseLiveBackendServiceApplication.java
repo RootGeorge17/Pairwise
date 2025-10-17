6c6311a96f325248362fca011bd7a328defcdfee
RootGeorge17 <george.sopov17@gmail.com>
2025-10-17 17:04:46 +0100

    Initial vite setup

--- backend/src/main/java/com/pairwiselive/backend/PairWiseLiveBackendServiceApplication.java
+++ backend/src/main/java/com/pairwiselive/backend/PairWiseLiveBackendServiceApplication.java
@@ -2,10 +2,18 @@ package com.pairwiselive.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class PairWiseLiveBackendServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PairWiseLiveBackendServiceApplication.class, args);
 	}
+
+    @GetMapping(value = "/")
+    public String hello() {
+        return "Hello World!";
+    }
 }
