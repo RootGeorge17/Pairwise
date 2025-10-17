e2fd5df4c23d53600b30c3de0a0da9c3a08d9404
RootGeorge17 <george.sopov17@gmail.com>
2025-10-17 15:30:36 +0100

    Initial Spring Boot setup

---
+++ backend/src/main/java/com/pairwiselive/backend/PairWiseLiveBackendServiceApplication.java
@@ -0,0 +1,11 @@
+package com.pairwiselive.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairWiseLiveBackendServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PairWiseLiveBackendServiceApplication.class, args);
+	}
+}
